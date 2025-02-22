335dde05e72720e4c7148d58fa3d04abfc76ac2b
destiny <1327599010@qq.com>
2025-02-23 06:23:27 +0800

    Lab 2 submission

--- Code/src/azure.ts
+++ Code/src/azure.ts
@@ -1,2 +1,2 @@
-export const KEY = "paste your KEY 1 here";
+export const KEY = "2gNrJAF4yvYaxVC9klppvoFWyQsSp1QS122huQdqHv1PoEiN0qszJQQJ99BBACi5YpzXJ3w3AAAYACOGZavm";
     
--- Code/src/types.ts
+++ Code/src/types.ts
@@ -4,6 +4,11 @@ import { AnyActorRef } from "xstate";
 export interface DMContext {
   spstRef: AnyActorRef;
   lastResult: Hypothesis[] | null;
+  person?: string; 
+  day?: string;
+  time?: string;
+  allDay?: boolean;
+  yesNo?: "yes" | "no";
 }
 
 export type DMEvents = SpeechStateExternalEvent | { type: "CLICK" };
