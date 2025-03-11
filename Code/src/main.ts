821fe0a7b17a9f23b72e8d27084523fd7e0ccb88
Vladislav Maraev <vlad@maraev.me>
2025-03-11 21:18:19 +0100

    Bump speechstate; remove unused files from build

--- Code/src/main.ts
+++ Code/src/main.ts
@@ -1,6 +1,4 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import { setupButton } from "./dm.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
