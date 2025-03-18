508ec501cadaaeec92713fd0ffd69e70d1ce0e9c
gong tianyi <137211917+destinywillso@users.noreply.github.com>
2025-03-18 16:02:36 +0800

    Update main.ts

--- Code/src/main.ts
+++ Code/src/main.ts
@@ -1,11 +1,7 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
-<<<<<<< HEAD
-import { setupButton } from "./dm4.js";
-=======
 import { setupButton } from "./dm.ts";
->>>>>>> 59cecbb90cd1cb7c68def12d975a13243584e990
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
